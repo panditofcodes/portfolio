096f6825d6600a716d7dc0d5a088b16d7b261289
Piyush Shukla <panditofcodes@gmail.com>
2023-08-19 16:32:24 +0530

    Basic structure is completed.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landingpage from "./component/Landingpage"; // Correct the import and case here
 
 function App() {
   return (
-    <>
-    <h1>Working</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landingpage />} /> {/* Correct the component name */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
