bc2518fc6c41f16ca70b908662f8b3b5daed25cf
Piyush Shukla <panditofcodes@gmail.com>
2023-08-21 16:42:18 +0530

    Contact page added.

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -2,12 +2,29 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="contact-container" data-aos="fade-down">
+    <div className="contact" data-aos="fade-down">
       <h2>Contact Me</h2>
-      <p>If you're interested in working with me or have any inquiries, feel free to get in touch!</p>
-      <a href="mailto:your.email@example.com" className="hire-me-button">
-        Hire Me
-      </a>
+      <p>
+        If you're interested in collaborating on a project, discussing potential
+        opportunities, or just want to say hi, feel free to get in touch with me
+        using the following methods:
+      </p>
+      <ul>
+        <li>Email: piyushshukla_@outlook.com</li>
+        <li>Phone: +919161741999</li>
+        <li>
+          LinkedIn:{" "}
+          <a href="https://www.linkedin.com/in/panditofcodes/">
+            Your LinkedIn Profile
+          </a>
+        </li>
+        <li>
+          GitHub:{" "}
+          <a href="https://github.com/panditofcodes">Your GitHub Profile</a>
+        </li>
+        {/* Add more contact methods if needed */}
+      </ul>
+      <button className="btn btn-primary">Hire Me</button>
     </div>
   );
 };
--- src/component/Landingpage.js
+++ src/component/Landingpage.js
@@ -15,7 +15,7 @@ export default function Landingpage() {
         <Home />
         <About />
         <Projects />
-        {/* <Contact /> */}
+        <Contact />
       </div>
     </>
   );
