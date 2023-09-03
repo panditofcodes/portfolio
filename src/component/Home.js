5bd106f1d96585036c2b6c04bdc92c9f3eadbd43
Piyush Shukla <panditofcodes@gmail.com>
2023-09-03 18:08:53 +0530

    Updated CSS

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,9 +2,8 @@ import React from "react";
 import { BsLinkedin, BsGithub, BsTwitter, BsInstagram } from "react-icons/bs";
 import resume from "./asset/Resume.pdf";
 import Lottie from "lottie-react";
-import down_arrow from "./asset/down_arrow.json"
-import welcome from "./asset/welcome.json"
-
+import down_arrow from "./asset/down_arrow.json";
+import welcome from "./asset/welcome.json";
 
 export default function Home() {
   const downloadResume = () => {
@@ -33,53 +32,46 @@ export default function Home() {
 
   return (
     <>
-    <Lottie animationData={welcome} className="welcome-animation" />
-      <div className="container mt-5 temp" data-aos="zoom-in">
-        <div className="row">
-          <div className="col-lg-6 col-md-8 col-sm-12 mx-auto">
-            <div className="button-container">
-              <button className= "btn-hireMe" onClick={hireMe}>
-                Hire Me
-              </button>
-              <button
-                onClick={downloadResume}
-                className="btn-resume"
-              >
-                Resume
-              </button>
-            </div>
-            <div className="text-center mb-4">
-              <h1>I'm Piyush Shukla</h1>
-              <p className="lead">
-                An aspiring full-stack MERN developer eager to explore web
-                development. I've delved into MongoDB, Express.js, React.js, and
-                Node.js, mastering these technologies. From crafting responsive
-                UIs with React to enhancing user experiences through efficient
-                solutions, I'm committed to continuous learning. Collaboration
-                is at my core, and I thrive in teams, bringing ideas to life.
-                Looking forward, I aim to contribute to impactful projects,
-                refine my skills, and embrace the dynamic realm of full-stack
-                MERN development.
-              </p>
-              <div className="social-links">
-                <a href="https://linkedin.com/in/panditofcodes">
-                  <BsLinkedin />
-                </a>
-                <a href="https://github.com/panditofcodes">
-                  <BsGithub />
-                </a>
-                <a href="https://twitter.com/piyush__shukla">
-                  <BsTwitter />
-                </a>
-                <a href="https://instagram.com/piyush._.shukla">
-                  <BsInstagram />
-                </a>
-              </div>
-            </div>
+      <div className="home-section">
+        <Lottie animationData={welcome} className="welcome-animation" />
+        <div className="button-container">
+          <button className="btn-hireMe" onClick={hireMe}>
+            Hire Me
+          </button>
+          <button onClick={downloadResume} className="btn-resume">
+            Resume
+          </button>
+        </div>
+        <div className="home-intro" data-aos="zoom-in">
+          <h1>I'm Piyush Shukla</h1>
+          <p className="lead">
+            An aspiring full-stack MERN developer eager to explore web
+            development. I've delved into MongoDB, Express.js, React.js, and
+            Node.js, mastering these technologies. From crafting responsive UIs
+            with React to enhancing user experiences through efficient
+            solutions, I'm committed to continuous learning. Collaboration is at
+            my core, and I thrive in teams, bringing ideas to life. Looking
+            forward, I aim to contribute to impactful projects, refine my
+            skills, and embrace the dynamic realm of full-stack MERN
+            development.
+          </p>
+          <div className="social-links">
+            <a href="https://linkedin.com/in/panditofcodes">
+              <BsLinkedin />
+            </a>
+            <a href="https://github.com/panditofcodes">
+              <BsGithub />
+            </a>
+            <a href="https://twitter.com/piyush__shukla">
+              <BsTwitter />
+            </a>
+            <a href="https://instagram.com/piyush._.shukla">
+              <BsInstagram />
+            </a>
           </div>
         </div>
+        <Lottie animationData={down_arrow} className="arrow-animation" />
       </div>
-      <Lottie animationData={down_arrow} className="arrow-animation"/>
     </>
   );
 }
